6fef456e22c9a8c8f68140b173bd32a0ffa9ebf8
omardoescode <omardoescode@gmail.com>
2025-05-29 08:39:14 +0300

    feat: Add cats dependency

--- build.sbt
+++ build.sbt
@@ -7,5 +7,8 @@ lazy val root = project
     name := "interpreter_strategy",
     version := "0.1.0-SNAPSHOT",
     scalaVersion := scala3Version,
-    libraryDependencies += "org.scalameta" %% "munit" % "1.0.0" % Test
+    libraryDependencies ++= Seq(
+      "org.scalameta" %% "munit" % "1.0.0" % Test,
+      "org.typelevel" %% "cats-core" % "2.13.0"
+    )
   )
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,37 +1,12 @@
-enum Regexp {
-  case Append(left: Regexp, right: Regexp)
-  case OrElse(first: Regexp, second: Regexp)
-  case Repeat(source: Regexp)
-  case Apply(string: String)
-  case Empty
+import cats.*
+import cats.syntax.all.*
 
-  def ++(that: Regexp): Regexp = Append(this, that)
-  def orElse(that: Regexp): Regexp = OrElse(this, that)
-  def repeat: Regexp = Repeat(this)
-  def `*` : Regexp = this.repeat
-
-  def matches(input: String): Boolean =
-    def loop(regexp: Regexp, idx: Int): Option[Int] =
-      regexp match {
-        case Append(left, right) =>
-          loop(left, idx).flatMap(i => loop(right, i))
-        case OrElse(first, second) =>
-          loop(first, idx).orElse(loop(second, idx))
-        case Repeat(source) =>
-          loop(source, idx).flatMap { i => loop(regexp, i) }.orElse(Some(idx))
-        case Apply(string) =>
-          Option.when(input.startsWith(string, idx))(idx + string.length)
-        case Empty => None
-      }
-
-    loop(this, 0).contains(input.length)
-}
-object Regexp {
-  val empty: Regexp = Empty
-
-  def apply(string: String): Regexp = Apply(string)
+case class Person(name: String, age: Int)
+object Person {
+  given Show[Person] with
+    def show(person: Person): String =
+      s"${person.name} is ${person.age} years old"
 }
 
 @main def run() =
-  val regexp = Regexp.apply("Scala").`*`
-  assert(regexp.matches("ScalaScala"))
+  println(Person("Omar", 20).show)
